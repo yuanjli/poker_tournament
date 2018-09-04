4f5055fa716580a52c238045617b7f57f629580d
yuanjli <liyuanjienetwork@gmail.com>
2018-09-03 21:25:54 -0700

    update code along version one

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //Requires
+var bodyParser = require("body-parser");
 var express = require("express");
 var ejsLayouts = require("express-ejs-layouts");
+// it includes the file teamService.js
+var teamsDB = require("./models/teamService.js");
 
 //Global vars
 var app = express();
@@ -8,11 +11,74 @@ var app = express();
 //Set and Use statements
 app.set("view engine", "ejs");
 app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extented: false}));
+app.use(express.static("public"));
 
 //Routes
 app.get("/", function(req, res){
   res.render("home");
 });
 
+app.get("/teams", function(req, res) {
+	// updates the all teams becuase it gets the all teams everytime:
+	var allTeams = teamsDB.getAllTeams();
+	//console.log('hi', allTeams)
+	//sending a messaging for checking the function is working: 
+	res.render("teams/index", {teams: allTeams});  //display the all teams
+});
+
+// : means it is a variable: 
+// app.get("/teams/:name", function(req, res){
+// 	//params is build in the express: 
+// 	var team = teamsDB.getOneTeam(req.params.name);
+// 	res.send(team);
+// });
+app.get("/teams/new", function(req, res){
+	res.render("teams/new");
+});
+
+app.get("/teams/:name", function(req, res){
+	//params is build in the express: 
+	var team = teamsDB.getOneTeam(req.params.name);
+	res.render("teams/show", {team: team});
+});
+
+
+app.post("/teams", function(req, res){
+	console.log(req.body);
+	teamsDB.addTeam(req.body);
+//  res.send("POST IS WORKING!");
+	res.redirect("/teams/" + req.body.name);
+});
+
+app.get("/teams/edit/:name", function(req, res){
+	var team = teamsDB.getOneTeam(req.params.name);
+	res.render("teams/edit", {team: team});
+});
+
+app.put("/teams/:name", function(req, res){
+	console.log(req.body);
+	teamsDB.editTeam(req.params.name, req.body);
+	res.send("PUUUUUUUUUUUUUT");
+});
+
+app.delete("/teams/:name", function(req, res){
+	teamsDB.deleteTeam(req.params.name);
+	res.send("DELETE!");
+});
+
+
+
+
 //Listen
 app.listen(3000);
+
+
+
+
+
+
+
+
+
+
--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,4 +1,47 @@
 $(document).ready(function() {
   console.log('Hello from teams-ajax.js!');
   // TO DO: Define calls to PUT and DELETE routes here
-});
+  
+  $("#edit-form").submit(function(e){
+  	e.preventDefault();
+
+  	console.log("edit form is submitted!");
+  	var teamUrl = $(this).attr("action");
+  	//grabing the data from the entrance of the form. 
+  	var teamData = $(this).serialize();
+  	
+  	console.log("url is ", teamUrl);
+  	console.log("data", teamData);
+  	
+  	$.ajax({
+  		method: "PUT", 
+  		url: teamUrl,
+  		data: teamData
+  	}).done(function(data){
+  		console.log("success!");
+  		// window.location = teamUrl; 
+  	}).fail(function(err){
+  		console.log("error", err);
+  	}); // end of AJAX
+  }); //end of edit-form submit 
+
+  $("#delete-btn").click(function(e){
+  	e.preventDefault();
+  	var teamUrl = $(this).attr("href");
+
+  	console.log("stuff is working, url is", teamUrl);
+
+  	$.ajax({
+  		method:"DELETE",
+  		url: teamUrl
+  	}).done(function(data){
+  		console.log("success", data);
+  		window.location = "/teams";
+  	}).fail(function(err){
+  		console.log("err", err);
+
+  	});   // end of AJAX
+
+  });  // End of Delete Button Click  
+
+}); // end of document ready;
